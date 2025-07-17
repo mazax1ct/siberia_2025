15df68982a8675851bfba33f65a720db8a29a3b4
mazax1ct <vova@giveandget.ru>
2025-07-17 20:12:39 +0300

    some blok

---
+++ build/js/scroller.js
@@ -0,0 +1,38 @@
+let scrollerSideOffset = 40;
+var scroller;
+
+$(document).ready(function() {
+    if ($('body').width() >= 1200) {
+        scrollerSideOffset = (window.innerWidth - 1170) / 2;
+    }
+
+    scroller = new Swiper('.js-scroller', {
+        loop: false,
+        freeMode: true,
+        grabCursor: true,
+        slidesPerView: 'auto',
+        spaceBetween: 16,
+        slidesOffsetAfter: 16,
+        slidesOffsetBefore: 16,
+        watchOverflow: true,
+
+        breakpoints: {
+            1200: {
+                spaceBetween: 20,
+                slidesOffsetAfter: scrollerSideOffset,
+                slidesOffsetBefore: scrollerSideOffset
+            }
+        }
+    });
+});
+
+$(window).on("resize", function() {
+    if ($('body').width() >= 1200) {
+        scrollerSideOffset = (window.innerWidth - 1170) / 2;
+        scroller.params.slidesOffsetAfter = scrollerSideOffset;
+        scroller.params.slidesOffsetBefore = scrollerSideOffset;
+        setTimeout(function() {
+            scroller.update();
+        }, 100);
+    }
+});
