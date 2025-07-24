78660d9dbf9c04da1d63f43988bce07d3a10adcc
mazax1ct <vova@giveandget.ru>
2025-07-24 16:39:12 +0300

    some pages

---
+++ build/js/simple-slider.js
@@ -0,0 +1,25 @@
+function addZero(num) {
+  return (num > 9) ? num : '0' + num;
+}
+
+$(document).ready(function() {
+  $('.js-simple-slider').each(function(index, el) {
+    new Swiper(el, {
+      loop: false,
+      spaceBetween: 16,
+      slidesPerView: 1,
+
+      pagination: {
+        el: '.js-simple-slider-pagination[data-slider="'+el.dataset.slider+'"]',
+        type: "fraction",
+        formatFractionCurrent: addZero,
+        formatFractionTotal: addZero
+      },
+
+      navigation: {
+        nextEl: '.js-simple-slider-next[data-slider="'+el.dataset.slider+'"]',
+        prevEl: '.js-simple-slider-prev[data-slider="'+el.dataset.slider+'"]',
+      },
+    });
+  });
+});
